27a633e9d2feb83f38dc3db11a8439c0f9a3fe60
Kevin Escobar <kesco025@jaguar.tamu.edu>
2025-09-10 17:38:40 +0000

    Comments Added to Lab

--- nLab3_Kevin_Escobar.java
+++ nLab3_Kevin_Escobar.java
@@ -5,26 +5,26 @@
 * Putting my skills to use and create my own coding workplace as well 
 * using formulas and Math.pow to enhance my coding further more 
 */
-import java.util.Scanner ;
+import java.util.Scanner ; // Imports the Scanner that we will be using 
 
 public class nLab3_Kevin_Escobar
 {
-    public static void main(String[] args)
+    public static void main(String[] args) //sets uo the coding workplace
     {
     Scanner input = new Scanner(System.in);
-    double initialValue ;
-    int years;
-    double interestRate;
-    double finalValue;
-    System.out.print(" Please enter the Initial Value of the home:");
-    initialValue = input.nextDouble();
-    System.out.print(" Please enter the amount of elapsed years:");
-    years = input.nextInt();
-    System.out.print(" Please enter the Interest Rate:");
-    interestRate = input.nextDouble();
-    interestRate = interestRate / 100;
-    finalValue = initialValue * Math.pow(1 + interestRate, years);
-    System.out.printf(" The Final Value of the home is: $%.2f%n", finalValue );
+    double initialValue ; // Delcaring variable
+    int years; // Declaring variable int due to it being a whole number
+    double interestRate; // Declaring variable
+    double finalValue;   // Declaring variable which is our final product
+    System.out.print(" Please enter the Initial Value of the home:"); // Ask user to imput intial value 
+    initialValue = input.nextDouble(); // Gives acces to user to imput a numerical value
+    System.out.print(" Please enter the amount of elapsed years:"); // Ask user to imput number of years
+    years = input.nextInt(); // Gives the ability to imput a numerical value 
+    System.out.print(" Please enter the Interest Rate:"); // Ask user to imput the interest rate
+    interestRate = input.nextDouble(); // Gives user acces to imput a value 
+    interestRate = interestRate / 100; // converts percentage to decial 
+    finalValue = initialValue * Math.pow(1 + interestRate, years); // Formula for compound interest 
+    System.out.printf(" The Final Value of the home is: $%.2f%n", finalValue ); // Sends out the final value with two decimal places 
 
 
 
